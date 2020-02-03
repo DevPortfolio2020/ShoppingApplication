a7c3744aa12d56d7fa15bcb1a23decc6a00b3fa5
DevPortfolio2020 <60587657+DevPortfolio2020@users.noreply.github.com>
2020-02-02 19:37:42 -0500

    Shopping app

---
+++ ShoppingConsole/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingConsole
+{
+    class Order
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public double PricePerLineItem { get; set; }
+        public double DiscountPerLineItem { get; set; }
+        public double TotalPricePerLineItem { get; set; } 
+        public double TotalDiscountPerLineItem { get; set; }
+    }
+}
